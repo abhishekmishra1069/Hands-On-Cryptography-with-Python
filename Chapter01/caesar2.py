032ba57ff8984763fc84926ffe63530ec822be18
abhishekmishra1069 <abhishekmishra1069@gmail.com>
2025-09-10 14:09:58 +0800

    Updating codes to handle user feedback on encode and decode

--- Chapter01/caesar2.py
+++ Chapter01/caesar2.py
@@ -1,7 +1,9 @@
+import sys
 alpha =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#alpha = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
 
-str_in = raw_input("Enter message, like HELLO: ")
-shift = int(raw_input("Shift value, like 3: "))
+str_in = input("Enter message, like HELLO: ")
+shift = int(input("Shift value, like 3: "))
 
 n = len(str_in)
 str_out = ""
@@ -10,7 +12,11 @@ for i in range(n):
    c = str_in[i]
    loc = alpha.find(c)
    newloc = loc + shift
+   print("i=", i, "c=", c, "loc=", loc, "newloc=", newloc)
    str_out += alpha[newloc]
 
-print "Obfuscated version:", str_out
+print ("Obfuscated version:", str_out)
 
+if __name__ == "__main__":
+      sys.exit(0)
+# end of file
--- Chapter01/caesar5.py
+++ Chapter01/caesar5.py
@@ -1,17 +1,30 @@
-alpha =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+import sys
+#alpha =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
 
-str_in = raw_input("Enter ciphertext: ")
 
-for shift in range(26):
 
+def encode(shift, str_in):
   n = len(str_in)
   str_out = ""
 
   for i in range(n):
     c = str_in[i]
     loc = alpha.find(c)
-    newloc = (loc + shift)%26
+    newloc = (loc + shift)%52
     str_out += alpha[newloc]
+    print("i=", i, "c=", c, "loc=", loc, "newloc=", newloc,   "char=", alpha[newloc]  )
 
-  print shift, str_out
+  return str_out
+
+def decode(shift, str_in):
+  return encode(-shift, str_in)
+
+if __name__ == "__main__": 
+  str_in = input("Enter ciphertext: ")
+  shift = int(input("Shift value, like 3: "))
+  str_out = encode(shift, str_in)
+  print ("Obfuscated version:", str_out)
+  str_back = decode(shift, str_out)
+  print ("De-obfuscated version:", str_back)
 
